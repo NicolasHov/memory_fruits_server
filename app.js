4d5f4a3f2a49b3ba7f29e1fa29b80aa176315414
Nicolas Hovart <hovart.nicolas@gmail.com>
2024-01-14 21:37:04 +0100

    Try to solve CSP error when deploying to netlify

--- app.js
+++ app.js
@@ -1,41 +1,41 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const cors = require('cors')
-require('dotenv').config();
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+// const cors = require('cors')
+// require('dotenv').config();
 
-const scoreRoutes = require('./routes/scores');
-const dev_db_url =
-    'mongodb+srv://Admin:' + process.env.MONGO_PASSWORD + '@cluster0.ypzya.mongodb.net/memory?retryWrites=true&w=majority';
-const mongoDB = process.env.MONGODB_URI || dev_db_url;
+// const scoreRoutes = require('./routes/scores');
+// const dev_db_url =
+//     'mongodb+srv://Admin:' + process.env.MONGO_PASSWORD + '@cluster0.ypzya.mongodb.net/memory?retryWrites=true&w=majority';
+// const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
-mongoose.connect(mongoDB,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
+// mongoose.connect(mongoDB,
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     })
+//     .then(() => console.log('Connexion à MongoDB réussie !'))
+//     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-const app = express();
+// const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-    console.log('development');
-}
+// if (process.env.NODE_ENV === 'development') {
+//     console.log('development');
+// }
 
-if (process.env.NODE_ENV === 'production') {
-    console.log('production');
-}
-app.use(cors())
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
+// if (process.env.NODE_ENV === 'production') {
+//     console.log('production');
+// }
+// app.use(cors())
+// app.use((req, res, next) => {
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+//     next();
+// });
 
-app.use(bodyParser.json());
+// app.use(bodyParser.json());
 
-app.use('/api/scores', scoreRoutes)
+// app.use('/api/scores', scoreRoutes)
 
-module.exports = app;
+// module.exports = app;
---
+++ functions/api.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const serverless = require('serverless-http')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors')
+require('dotenv').config();
+
+const scoreRoutes = require('../routes/scores');
+
+// const exp = require('constants');
+
+
+//  ######### Connection mongo #########
+const dev_db_url =
+    'mongodb+srv://Admin:' + process.env.MONGO_PASSWORD + '@cluster0.ypzya.mongodb.net/memory?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
+mongoose.connect(mongoDB,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+//  ######### END connection mongo #########
+
+const app = express();
+const router = express.Router()
+
+if (process.env.NODE_ENV === 'development') {
+    console.log('development');
+}
+
+if (process.env.NODE_ENV === 'production') {
+    console.log('production');
+}
+router.use(cors())
+router.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+router.use(bodyParser.json());
+
+app.get('/', (req, res, next) => {
+    res.send('home')
+});
+router.use('/api/scores', scoreRoutes)
+
+app.use('./netlify/functions/api', router)
+module.exports.handler = serverless(app);
